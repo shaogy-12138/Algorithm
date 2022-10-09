058f57aa798791c7c8709d430dc21f9851d24c95
shaogy-12138 <shaogy12138@163.com>
2022-10-09 20:04:56 +0800

    20221009

---
+++ 02.算法入门/01_max_value.cpp
@@ -0,0 +1,38 @@
+/*
+ * @Author : shaogy
+ * @File : 01_max_value
+ * @Date : 9/10/2022
+ * @Description ://TODO 
+**/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    vector<int>w {4, 10, 7, 11, 3, 5, 14, 2};
+    for( auto i = w.begin(); i != w.end(); i++){
+        cout<<*i<<' ';
+    }
+    cout<<endl;
+    sort(w.begin(), w.end());
+    for( auto i = w.begin(); i != w.end(); i++){
+        cout<<*i<<' ';
+    }
+    cout<<endl;
+
+    int c_max = 30, sum = 0;
+    int i;
+    for(i = 0; i < w.size(); i++){
+        sum += w[i];
+        if(sum > c_max){
+            i--;
+            break;
+        }else if(sum == c_max){
+            break;
+        }
+    }
+    return 0;
+}
